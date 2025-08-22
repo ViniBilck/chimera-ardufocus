561f686a9fa6f6e21c2edacabc0c3166692c9819
vinicius <bilck.vinicius1998@gmail.com>
2025-08-21 21:48:13 -0300

    template files

---
+++ chimera_ardufocus/instruments/ardufocus.py
@@ -0,0 +1,21 @@
+import logging
+import sys
+
+from chimera.core.lock import lock
+from chimera.interfaces.focuser import FocuserFeature, InvalidFocusPositionException, FocuserAxis
+from chimera.instruments.focuser import FocuserBase
+from chimera.core.chimeraobject import ChimeraObject
+
+class ARDUFocus(ChimeraObject):
+    __config__ = {"param1": "a string parameter"}
+
+    def __init__(self):
+        ChimeraObject.__init__(self)
+
+    def __start__(self):
+        self.doSomething("test argument")
+
+    def doSomething(self, arg):
+        self.log.warning("Hi, I'm doing something.")
+        self.log.warning("My arg=%s" % arg)
+        self.log.warning("My param1=%s" % self["param1"])
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='chimera_ardufocus',
+    version='0.0.1',
+    packages=['chimera_ardufocus', 'chimera_ardufocus.instruments'],
+    scripts=[],
+    url='http://github.com/vinibilck/chimera-ardufocus',
+    license='GPL v2',
+    author='Vinicius Bilck',
+    author_email='v.bilck@grad.ufsc.br',
+    description='Template for chimera plugins'
+)
+
